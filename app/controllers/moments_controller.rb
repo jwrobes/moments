ecd72791c8924cf55937909e7e292de59c1dc2b6
Jonathan Wrobel <jwrobes@gmail.com>
2014-05-31 08:36:14 -0700

    Added define a time controller route to Momemnts Controller that allows user to set time when moments start and stop 	*removed from user registration to allow for easier change

--- app/controllers/moments_controller.rb
+++ app/controllers/moments_controller.rb
@@ -3,6 +3,18 @@ class MomentsController < ApplicationController
 
   end
 
+  def time
+  	start_of_day = Time.parse("00:00")
+  	start_time = Time.parse(params["start_time"]) - start_of_day 
+  	end_time = Time.pa
+  	current_user.start_time = start_time
+  	current_user.end_time = end_time
+  	current_user.save
+  	first_time =  Time.parse("00:00").to_i + rand(start_time..end_time)
+  	puts Time.at(first_time)
+  	redirect_to '/'
+
+  end
  
 end
 
