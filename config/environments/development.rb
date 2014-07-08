bb5240f8ac10cae2f2a96db14bd6f8ae3cbc458c
Jonathan Wrobel <jwrobes@gmail.com>
2014-07-08 11:51:45 -0700

    added the redistogo_url from heroku to development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
 
+
+ENV["REDISTOGO_URL"] = 'redis://redistogo:bdfbdffe08cac827fa5f782cd4ad5cff@grideye.redistogo.com:10898/'
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
