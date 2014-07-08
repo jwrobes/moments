4cc6e82c94ffbef16909694faf08c6ad09e2b4f0
Jonathan Wrobel <jwrobes@gmail.com>
2014-07-08 12:39:43 -0700

    Fix reference to .env variables using the ENV hash

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -11,9 +11,9 @@ class TwilioController < ApplicationController
 	def send_text_message
 		number_to_send_to = params[:number_to_send_to]
 		
-		@twilio_client = Twilio::REST::Client.new TWILIO_SID, TWILIO_TOKEN
+		@twilio_client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
 		@twilio_client.account.sms.message.create(
-			:from => "+1#{TWILIO_PHONE_NUMBER}",
+			:from => "+1#{ENV['TWILIO_PHONE_NUMBER']}",
 			:to => number_to_send_to,
 			:body => "This is an message. It gets send to"
 			)
--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -18,11 +18,8 @@ class TwilioController < ApplicationController
 	def send_sms
 		message = params[:message]
 		number = params[:number]
-		account_sid = "AC90d2dcc4e6f5a0b4cf58dbb6008bbdc4"
-		auth_token = "a58cd6e9e716fa2446636f6a8b4087db"
-		twilio_phone_number = "5106296985"
-		@client = Twilio::REST::Client.new account_sid, auth_token
-		@message = @client.account.messages.create({:to=>"+1"+"#{number}", :from =>"1"+twilio_phone_number, :body => "#{message}" })
+		@client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
+		@message = @client.account.messages.create({:to=>"+1"+"#{number}", :from =>"1"+ENV['TWILIO_PHONE_NUMBER'], :body => "#{message}" })
 		redirect_to '/'
 	end
 end
