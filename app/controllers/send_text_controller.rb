5468fe41719bada4ed374ad3e3f37409e0deb8bb
Jonathan Wrobel <jwrobes@gmail.com>
2014-07-08 10:39:47 -0700

    Removed old Twilio API Keys from controller 	*changed text controller back to checking for moments every minute

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -11,13 +11,9 @@ class TwilioController < ApplicationController
 	def send_text_message
 		number_to_send_to = params[:number_to_send_to]
 		
-		twilio_sid = "PN523cf9642e9179e2dd3d78040934e993"
-		twilio_token = "a58cd6e9e716fa2446636f6a8b4087db"
-		twilio_phone_number = "5106296985"
-
-		@twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+		@twilio_client = Twilio::REST::Client.new TWILIO_SID, TWILIO_TOKEN
 		@twilio_client.account.sms.message.create(
-			:from => "+1#{twilio_phone_number}",
+			:from => "+1#{TWILIO_PHONE_NUMBER}",
 			:to => number_to_send_to,
 			:body => "This is an message. It gets send to"
 			)
