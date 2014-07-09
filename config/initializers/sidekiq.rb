2a750aaa6cddc7a44c059500628bcecb588d0ea2
Jonathan Wrobel <jwrobes@gmail.com>
2014-07-09 09:30:00 -0700

    fixing REDIS URL to REDISTOGO

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
-ENV["REDIS_URL"] ||= "redis://localhost:6379"
+ENV["REDISTOGO_URL"] ||= "redis://localhost:6379"
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDIS_URL"], namespace: 'sidekiq' }
+  config.redis = { url: ENV["REDISTOGO_URL"], namespace: 'sidekiq' }
 end
 
 unless Rails.env.production?
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV["REDIS_URL"], namespace: 'sidekiq'  }
+    config.redis = { url: ENV["REDISTOGO_URL"], namespace: 'sidekiq'  }
   end
 end
