33c2bd3cd157104902040f2da5874652eac37e76
Jonathan Wrobel <jwrobes@gmail.com>
2014-07-22 00:30:41 -0700

    Remove the two workers for sidekiq and sidetiq
    TODO: update both workers for Resque

--- app/workers/backup_moments_scheduler.rb
+++ app/workers/backup_moments_scheduler.rb
@@ -1,20 +1,20 @@
-class BackupMomentsScheduler
-	include Sidekiq::Worker
-	include Sidetiq::Schedulable
-	# include Moments::Checker
+# class BackupMomentsScheduler
+# 	include Sidekiq::Worker
+# 	include Sidetiq::Schedulable
+# 	# include Moments::Checker
 
-recurrence { hourly.minute_of_hour(20, 30, 40, 50)  }
+# recurrence { hourly.minute_of_hour(20, 30, 40, 50)  }
 
- def perform
- 	puts "Yeah Baby check for those moments"
- 	current_utc_local_midnight = Time.zone.now.hour
- 	users_at_or_after_midnight_locally = User.where("utc_local_midnight >= ?", current_utc_local_midnight)
- 	users_with_moments_today = User.users_with_moments_today
- 	users_missing_moments = users_at_or_after_midnight_locally - users_with_moments_today
- 	users_missing_moments.each do |user| 
- 		puts "build the moment for #{user.email}"
- 		Moment.generate_moments_for_day(user) 
- 		end
- end
+#  def perform
+#  	puts "Yeah Baby check for those moments"
+#  	current_utc_local_midnight = Time.zone.now.hour
+#  	users_at_or_after_midnight_locally = User.where("utc_local_midnight >= ?", current_utc_local_midnight)
+#  	users_with_moments_today = User.users_with_moments_today
+#  	users_missing_moments = users_at_or_after_midnight_locally - users_with_moments_today
+#  	users_missing_moments.each do |user| 
+#  		puts "build the moment for #{user.email}"
+#  		Moment.generate_moments_for_day(user) 
+#  		end
+#  end
 
-end
+# end
--- app/workers/moments_scheduler.rb
+++ app/workers/moments_scheduler.rb
@@ -1,17 +1,17 @@
-class MomentsScheduler
-	include Sidekiq::Worker
-	include Sidetiq::Schedulable
-	# include Moments::Checker
+# class MomentsScheduler
+# 	include Sidekiq::Worker
+# 	include Sidetiq::Schedulable
+# 	# include Moments::Checker
 
-recurrence { hourly }
+# recurrence { hourly }
 
- def perform
- 	puts "Yeah Baby build those moments"
- 	current_utc_local_midnight = Time.zone.now.hour
+#  def perform
+#  	puts "Yeah Baby build those moments"
+#  	current_utc_local_midnight = Time.zone.now.hour
  	
- 	users_at_midnight_locally = User.where("utc_local_midnight = ?", current_utc_local_midnight)
- 	puts users_at_midnight_locally
- 	users_at_midnight_locally.each { |user| Moment.generate_moments_for_day(user) }
- end
+#  	users_at_midnight_locally = User.where("utc_local_midnight = ?", current_utc_local_midnight)
+#  	puts users_at_midnight_locally
+#  	users_at_midnight_locally.each { |user| Moment.generate_moments_for_day(user) }
+#  end
 
-end
+# end
