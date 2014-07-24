89a61fa208696ed4e5b74fd869470d7a5c03f04c
Jonathan Wrobel <jwrobes@gmail.com>
2014-07-24 08:13:36 -0700

    Added User Show page Timepicker class to user with pickadate gem

--- app/assets/javascripts/time.js
+++ app/assets/javascripts/time.js
@@ -3,10 +3,12 @@ var Time = {
 	init: function () {
 		$('#time-setups').on('ajax:success', this.appendTime);
 		$('#time-setups').on('ajax:error', this.appendError);
+		$('.start_time').pickatime()
+		$('.end_time').pickatime()
 	},
 	appendTime: function (e,data) {
-		$('#start_time').replaceWith('<h3 id="start_time"> Selected Start Time: ' + data.start_time)
-		$('#end_time').replaceWith('<h3 id="end_time"> Selected End Time: ' + data.end_time)
+		$('#start_time').replaceWith('<h3 id="start_time">Start Time: ' + data.start_time)
+		$('#end_time').replaceWith('<h3 id="end_time">End Time: ' + data.end_time)
 	},
 	appendError: function (e,data) {
 		console.log(data)
