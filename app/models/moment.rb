2252dab8e8b410f99acee8802f84d6e83896b65e
Jonathan Wrobel <jwrobes@gmail.com>
2014-07-27 12:14:10 -0700

    removed downloads

--- app/models/moment.rb
+++ app/models/moment.rb
@@ -9,7 +9,7 @@ belongs_to :user
 		daily_moment_times.each do |time|
 			local_time = TZInfo::Timezone.get(user.time_zone).utc_to_local(time)
 			formatted_local_time = local_time.strftime("%l:%M %p")
-			moment = self.new({date: Date.today, message: "Hey there #{user.email} take a moment now at #{formatted_local_time}", phone_number: user.phone_number, time: time})
+			moment = self.new({date: Date.today, message: "Hey there #{user.email} take a moment now at #{formatted_local_time} for today's date: #{Date.today}", phone_number: user.phone_number, time: time})
 			moment.save
 			puts "building another moment"
 			puts moment
